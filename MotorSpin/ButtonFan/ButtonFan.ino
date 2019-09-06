7d0a96748f518830e590f45783a512836c0b9b7b
jp-niehls <jpniehls@mail.lipscomb.edu>
2019-09-06 01:24:29 -0500

    FINAL DRAFT: Changed comments and cleaned up code. Added summary of code at the top.

--- MotorSpin/ButtonFan/ButtonFan.ino
+++ MotorSpin/ButtonFan/ButtonFan.ino
@@ -1,26 +1,46 @@
+/*
+ * 
+ * This code is a modified version of the code found in tutorial 
+ * lesson 19 "A small fan" on the SUNFOUNDER website:
+ * https://www.sunfounder.com/learn/Super-Kit-V2-0-for-Arduino/lesson-19-simple-creation-small-fan-super-kit.html
+ * 
+ * This code allows two pushbuttons to be used to change the speed and direction
+ * of an attached fan motor. 
+ * The speed pushbutton(buttonPin) changes the speed from off,
+ * to high, to low, then back off again. The direction 
+ * pushbutton(buttonPin2) changes the direction of the spinning fan.
+ * When both buttons are pushed simultaneously, the fan stops and 
+ * resets its speed and direction to the default settings.
+ * 
+ * Last edited by:  Jeremiah P. Niehls
+ * Date:            9/6/2019
+ * 
+ */
 const int buttonPin = D4; // the number of the pushbutton pin for speed
 const int buttonPin2 = D8;// the number of the pushbutton pin for direction
 const int motorIn1 = D6;
 const int motorIn2 = D5;
+
 int stat = 0;
 int stat2 = 0; 
+
 #define rank1 800
 #define rank2 400
+
 // Variables will change:
-int ledState = HIGH; // the current state of the output pin
-int buttonState; // the current reading from the input pin
-int lastButtonState = LOW; // the previous reading from the input pin
-int buttonState2; // the current reading from the input pin
+int buttonState;            // the current reading from the input pin
+int lastButtonState = LOW;  // the previous reading from the input pin
+int buttonState2;           // the current reading from the input pin
 int lastButtonState2 = LOW; // the previous reading from the input pin
 
 // the following variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
-long lastDebounceTime = 0; // the last time the output pin was toggled
-long debounceDelay = 50; // the debounce time; increase if the output flickers
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
 /******************************************************************************/
 void setup() 
 {
-//set theled,motors as OUTPUT,button as INPUT
+//set the motors as OUTPUT,button as INPUT
 pinMode(buttonPin, INPUT);
 pinMode(buttonPin2, INPUT);
 
@@ -41,10 +61,6 @@ if (reading != lastButtonState) {
 // reset the debouncing timer
 lastDebounceTime = millis();
 }
-/*if (reading2 != lastButtonState2) {
-// reset the debouncing timer
-lastDebounceTime = millis();
-}*/
 
 if ((millis() - lastDebounceTime) > debounceDelay) {
 // whatever the reading is at, it's been there for longer
@@ -57,8 +73,8 @@ buttonState = reading;
 buttonState2 = reading2;
 stat = 0;
 stat2 = 0;
-//}
 }
+
 // if pushbutton for speed has been pressed
 // increase speed variable or reset to 0 if it goes above 2
 if (reading != buttonState) {
@@ -118,7 +134,7 @@ lastButtonState = reading;
 lastButtonState2 = reading2;
 }
 /***********************************************************/
-void clockwise(int Speed)//
+void clockwise(int Speed)
 {
 analogWrite(motorIn1,0);
 analogWrite(motorIn2,Speed);
